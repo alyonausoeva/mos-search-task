963e2ef70ccf5b86e6a7442b16bbeb7c3d946860
alyonausoeva <alyonausus@gmail.com>
2021-08-07 22:26:01 +0300

    Updates

---
+++ precache-manifest.ded063a48a32f6e74608335395ad1445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0a112c0f78b7b159033cfae5964e2b",
+    "url": "/mos-search-task/index.html"
+  },
+  {
+    "revision": "e6abc005981206f344d7",
+    "url": "/mos-search-task/static/css/main.32689c2d.chunk.css"
+  },
+  {
+    "revision": "e243fd1f51e2021cf368",
+    "url": "/mos-search-task/static/js/2.a2ae3689.chunk.js"
+  },
+  {
+    "revision": "e6abc005981206f344d7",
+    "url": "/mos-search-task/static/js/main.6ef4aa43.chunk.js"
+  },
+  {
+    "revision": "12f0bc03d14dec5ff07b",
+    "url": "/mos-search-task/static/js/runtime-main.7dfc8dd5.js"
+  },
+  {
+    "revision": "c1782b8174b215799a042cf3903320e5",
+    "url": "/mos-search-task/static/media/cross.c1782b81.svg"
+  }
+]);
